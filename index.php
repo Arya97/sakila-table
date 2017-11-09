b45d15775f4389257257d7613050d70cadc609c5
Arya Wirasandi <aryawirasandi7153@gmail.com>
2017-11-09 17:30:45 +0700

    searching bar

--- index.php
+++ index.php
@@ -38,25 +38,59 @@ $films = mysqli_query($koneksi, 'SELECT*FROM film');
   <body>
     <h1>List Film Yang ada pada malam ini</h1>
     <div class="find">
-        <form action="index.php" method="post">
+        <form action="index.php" method="get">
             <div class="container">
                 <div class="row center">
-                    <div class="col s6 col">
-                        <input type="text" name="search" placeholder="Mencari?">
+                    <div class="col s6 ">
+                        <input type="text" name="cari" placeholder="Mencari?">
                     </div>
                     <div class="col s2">
-                        <input type="submit" name="btn"" value="Search" class="waves-effect waves-light btn">
+                        <input type="submit" name="cari" value="Search" class="waves-effect waves-light btn">
                     </div>
                 </div>
-
-
             </div>
-
         </form>
+        <?php
+            if(isset($_GET['cari'])){
+                $cari = $_GET['cari'];
+                echo "<b>Hasil Pencarian : </b>".$cari."</b>";
+            }
+        ?>
     </div>
     <br/>
-  <?php
-    include "tabel.php";
-  ?>
+    <div class="container">
+        <table border="2" width = '50%;' align = 'center' height="25%" class="striped bordered">
+            <thead>
+            <td>ID</td>
+            <td>JUDUL</td>
+            <td>DESKRIPSI</td>
+            <td>TAHUN</td>
+            <td>BAHASA</td>
+            <td>RATING</td>
+            <td>LAMA SEWA</td>
+            <td>BIAYA SEWA</td>
+            </thead>
+            <?php
+                if(isset($_GET['cari'])){
+                    $cari = $_GET['cari'];
+                    $data = mysql_query("SELECT * FROM film WHERE title like '%".$cari."%'");
+                }else{
+                    $data = mysql_query("SELECT * FROM film");
+                }
+            ?>
+            <?php foreach($films as $film) : ?>
+            <tbody>
+            <td><?= $film['film_id']?></td>
+            <td><?= $film['title']?></td>
+            <td><?= $film['description']?></td>
+            <td><?= $film['release_year']?></td>
+            <td><?= $film['language_id']?></td>
+            <td><?= $film['rental_rate']?></td>
+            <td><?= $film['rental_duration']?></td>
+            <td><?= $film['replacement_cost']?></td>
+            <tbody>
+            <?php endforeach;?>
+        </table>
+    </div>
   </body>
 </html>
